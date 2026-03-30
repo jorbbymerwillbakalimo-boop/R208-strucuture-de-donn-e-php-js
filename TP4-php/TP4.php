bcf8113949aa5aed43eccfa6bab74ef0a71d9b16
jorbbymerwillbakalimo-boop <jorbby.merwill.bakalimo@gmail.com>
2026-03-30 13:18:45 -0300

    Add files via upload

---
+++ TP4-php/TP4.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+  class Stock {
+    public $products;
+    // 1.2 Constructeur 
+
+    public function __construct(){
+      $this->products = [];
+    }
+  }
+   public function loadFromYamlFile($file =
+    "stock.yaml"){
+
+    $yaml = file_get_contents($file);
+    $data = yaml_parse($yaml);
+    foreach ($data as $product){
+      $ref = $product["ref"];
+      $this->products["ref"] = [
+        "name" => $product["name"],
+        "brand" => $product["brand"],
+        "price" => $product["price"],
+        "quantity" => 
+        $product ["quantity"]
+      ];
+    }
+  }
+
+?>
