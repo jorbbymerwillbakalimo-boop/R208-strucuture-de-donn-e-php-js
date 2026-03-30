474c19014b16b42caad485f835beb5769718729f
jorbbymerwillbakalimo-boop <jorbby.merwill.bakalimo@gmail.com>
2026-03-30 14:45:32 -0300

    Refactor loadFromYamlFile and add saveToYamlFile
    Refactor YAML loading and saving methods in Stock class.

--- TP4-php/TP4.php
+++ TP4-php/TP4.php
@@ -1,85 +1,58 @@
+#!/usr/bin/php
 <?php
 
 class Stock {
-
     public $products;
 
-    // Constructeur
     public function __construct() {
         $this->products = [];
     }
 
-    // Charger depuis YAML
     public function loadFromYamlFile($file = "stock.yaml") {
-
-        $yaml = file_get_contents($file);
-        $data = yaml_parse($yaml);
-
-        foreach ($data as $product) {
-
-            $ref = $product["ref"];
-
-            $this->products[$ref] = [
-                "name" => $product["name"],
-                "brand" => $product["brand"],
-                "price" => $product["price"],
-                "quantity" => $product["quantity"]
-            ];
+        $content = file_get_contents($file);
+        $pos = 0;
+        while (($data = @yaml_parse($content, $pos)) !== false) {
+            if (is_array($data) && isset($data["reference"])) {
+                $ref = $data["reference"];
+                $this->products[$ref] = [
+                    "name"             => $data["name"],
+                    "shortDescription" => $data["shortDescription"],
+                    "brand"            => $data["brand"],
+                    "price"            => $data["price"],
+                    "quantity"         => $data["quantity"]
+                ];
+            }
+            $pos++;
         }
     }
 
-    // Affichage
-    public function __toString() {
+    public function saveToYamlFile($file = "export.yaml") {
+        $yaml = "";
 
-        $output = "=== STOCK ===\n";
+        // Parcourir chaque produit du tableau associatif
+        foreach ($this->products as $product) {
+            // Convertir le produit en YAML
+            $block = yaml_emit($product);
 
-        foreach ($this->products as $ref => $p) {
-            $output .= $p["name"] . " (" . $p["brand"] . ")\n";
-            $output .= "Ref: $ref | Prix: " . $p["price"] . "€ | Qté: " . $p["quantity"] . "\n\n";
+            // yaml_emit() génère "--- \n...\n" on supprime le "..."
+            $block = preg_replace('/^\.\.\.\s*$/m', '', $block);
+
+            // Ajouter le bloc au résultat
+            $yaml .= $block;
         }
 
-        return $output;
-    }
+        // Ajouter le "..." final obligatoire en fin de fichier YAML
+        $yaml .= "...\n";
 
-    // Ajouter produit
-    public function addProductToStock($ref, $name, $brand, $price, $quantity = 1) {
-
-        $this->products[$ref] = [
-            "name" => $name,
-            "brand" => $brand,
-            "price" => $price,
-            "quantity" => $quantity
-        ];
-    }
-
-    // Supprimer produit
-    public function removeProductFromStock($ref) {
-
-        if (isset($this->products[$ref])) {
-            unset($this->products[$ref]);
-        }
-    }
-
-    // Ajouter quantité
-    public function addQuantityToProduct($ref, $q = 1) {
-
-        if (isset($this->products[$ref])) {
-            $this->products[$ref]["quantity"] += $q;
-        }
-    }
-
-    // Retirer quantité
-    public function removeQuantityFromProduct($ref, $q = 1) {
-
-        if (!isset($this->products[$ref])) return false;
-
-        if ($this->products[$ref]["quantity"] >= $q) {
-
-            $this->products[$ref]["quantity"] -= $q;
-            return true;
-
-        } else {
-            return false;
-        }
+        // Écrire dans le fichier
+        file_put_contents($file, $yaml, LOCK_EX);
     }
 }
+
+// Test
+$stock = new Stock();
+$stock->loadFromYamlFile("stock.yaml");
+$stock->saveToYamlFile("export.yaml");
+
+// Vérifier le résultat
+echo file_get_contents("export.yaml");
