9806d1bbe01c66221eb72ed12c7bdcd1e35cd5cb
jorbbymerwillbakalimo-boop <jorbby.merwill.bakalimo@gmail.com>
2026-03-30 13:35:59 -0300

    Fix formatting and indentation in Stock class

--- TP4-php/TP4.php
+++ TP4-php/TP4.php
@@ -1,28 +1,25 @@
 #!/usr/bin/php
 <?php
-  class Stock {
+
+class Stock {
     public $products;
-    // 1.2 Constructeur 
 
-    public function __construct(){
-      $this->products = [];
+    public function __construct() {
+        $this->products = [];
     }
-  }
-   public function loadFromYamlFile($file = "stock.yaml") {
 
-    $yaml = file_get_contents($file);
-
-    $data = yaml_parse($yaml);
-
-    foreach ($data as $product) {
-
-        $ref = $product["ref"];
-
-        $this->products[$ref] = [
-            "name" => $product["name"],
-            "brand" => $product["brand"],
-            "price" => $product["price"],
-            "quantity" => $product["quantity"]
-        ];
+    public function loadFromYamlFile($file = "stock.yaml") {
+        $yaml = file_get_contents($file);
+        $data = yaml_parse($yaml);
+        foreach ($data as $product) {
+            $ref = $product["ref"];
+            $this->products[$ref] = [
+                "name"     => $product["name"],
+                "brand"    => $product["brand"],
+                "price"    => $product["price"],
+                "quantity" => $product["quantity"]
+            ];
+        }
     }
 }
+?>
