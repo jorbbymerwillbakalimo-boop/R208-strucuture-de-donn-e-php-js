0a17a968dedf7cbd7b3e5aa7acf554c32ae67fb5
jorbbymerwillbakalimo-boop <jorbby.merwill.bakalimo@gmail.com>
2026-03-30 13:22:09 -0300

    Update TP4.php

--- TP4-php/TP4.php
+++ TP4-php/TP4.php
@@ -8,21 +8,21 @@
       $this->products = [];
     }
   }
-   public function loadFromYamlFile($file =
-    "stock.yaml"){
+   public function loadFromYamlFile($file = "stock.yaml") {
 
     $yaml = file_get_contents($file);
-    $data = yaml_parse($yaml);
-    foreach ($data as $product){
-      $ref = $product["ref"];
-      $this->products["ref"] = [
-        "name" => $product["name"],
-        "brand" => $product["brand"],
-        "price" => $product["price"],
-        "quantity" => 
-        $product ["quantity"]
-      ];
-    }
-  }
 
-?>
+    $data = yaml_parse($yaml);
+
+    foreach ($data as $product) {
+
+        $ref = $product["ref"];
+
+        $this->products[$ref] = [
+            "name" => $product["name"],
+            "brand" => $product["brand"],
+            "price" => $product["price"],
+            "quantity" => $product["quantity"]
+        ];
+    }
+}
